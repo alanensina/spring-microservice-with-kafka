358fc1b2ac07050341c4ba629eae7b33001fda8e
Alan Ensina <alanvinicius.ce@gmail.com>
2025-03-19 14:03:48 +0000

    Started Order api

---
+++ order/src/main/java/com/alanensina/order/controllers/OrderController.java
@@ -0,0 +1,14 @@
+package com.alanensina.order.controllers;
+
+import com.alanensina.order.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+}
---
+++ order/src/main/java/com/alanensina/order/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.alanensina.order.repositories;
+
+import com.alanensina.order.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface OrderRepository extends JpaRepository<Order, UUID> {
+}
