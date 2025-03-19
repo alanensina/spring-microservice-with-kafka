7793b06d93a05e223a94566cd83700850a06a76d
Alan Ensina <alanvinicius.ce@gmail.com>
2025-03-19 22:45:51 +0000

    Project restarted.

---
+++ email-service/src/test/java/com/alanensina/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alanensina.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
